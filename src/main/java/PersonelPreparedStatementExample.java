08971264498e7ee7e4f7b375aae5820c4df69ccd
Mustafa Sengul <Mustafa.Sengul@logo.com.tr>
2025-11-27 11:10:05 +0300

    first commit

---
+++ src/main/java/PersonelPreparedStatementExample.java
@@ -0,0 +1,16 @@
+package PACKAGE_NAME;
+
+ /**
+ * -----------------------------------------------------------------------------
+ * CLASS NAME   : PersonelPreparedStatementExample
+ * AUTHOR       : Mustafa.Sengul
+ * CREATED DATE : 26.11.2025
+ * DESCRIPTION  : [Kısa açıklama yazılabilir]
+ * -----------------------------------------------------------------------------
+ * UPDATE LOG   : 
+ *   - [TARİH] : [KULLANICI ADI] - [Yapılan değişiklik açıklaması]
+ * -----------------------------------------------------------------------------
+ */
+ 
+public class PersonelPreparedStatementExample {
+}
---
+++ src/main/java/RemotePersonelJDBIExample.java
@@ -0,0 +1,16 @@
+package PACKAGE_NAME;
+
+ /**
+ * -----------------------------------------------------------------------------
+ * CLASS NAME   : RemotePersonelJDBIExample
+ * AUTHOR       : Mustafa.Sengul
+ * CREATED DATE : 27.11.2025
+ * DESCRIPTION  : [Kısa açıklama yazılabilir]
+ * -----------------------------------------------------------------------------
+ * UPDATE LOG   : 
+ *   - [TARİH] : [KULLANICI ADI] - [Yapılan değişiklik açıklaması]
+ * -----------------------------------------------------------------------------
+ */
+ 
+public class RemotePersonelJDBIExample {
+}
